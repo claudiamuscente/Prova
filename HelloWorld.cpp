3156e70340bfae464461ad5dcc50ecc4f54f812c
claudiamuscente <muscente.claudia@gmail.com>
2018-12-26 19:29:58 +0100

    Aggiunto calcolo area

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+float calcolaArea(int base, int altezza);
+
 int main(){
 	
-	cout<<"Hello world";
+	cout<<"Hello world\n";
+	
+	int area = calcolaArea(5, 3);
+	cout<<"Area: "<<area;
+	
+}
+
+
+float calcolaArea(int base, int altezza){
+	return base * altezza / 2;
 }
